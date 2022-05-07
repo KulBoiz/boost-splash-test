04d420d246c3c6f9d239ea5179f1b79bfa93e3f3
LuuVanTruong308 <48488891+LuuVanTruong308@users.noreply.github.com>
2022-05-07 18:05:25 +0700

    add load more

--- app/screens/loan/records-management.tsx
+++ app/screens/loan/records-management.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { View, FlatList } from "react-native"
 import FinanceFilter from "./components/finance-filter"
 import { PROFILE_MANAGEMENT_FILTER } from "./constants"
@@ -9,24 +9,42 @@ import { useStores } from "../../models"
 import { AppText } from "../../components/app-text/AppText"
 import { fontFamily } from "../../constants/font-family"
 import { FONT_BOLD_12 } from "../../styles/common-style"
+import { observer } from "mobx-react-lite"
 
-interface Props{}
-const RecordsManagement = React.memo((props: Props) => {
-  const {loanStore} = useStores()
-  const data = loanStore?.records?.data ?? []
-  const total = loanStore?.records?.total ?? 0
+interface Props { }
+const RecordsManagement = observer((props: Props) => {
+  const { loanStore } = useStores()
+  const data = loanStore?.records ?? []
+  const total = loanStore?.total ?? 0
   const [select, setSelect] = useState<number>(0)
+  const [loadMore, setLoadMore] = useState<boolean>(false)
 
-  const renderItem = useCallback(({item}) => {
-      return <ShortStatus item={item} />
+  useEffect(()=> {
+    loanStore.getAllData()
   },[])
 
-  console.log(data)
+  const renderItem = useCallback(({ item }) => {
+    return <ShortStatus item={item} />
+  }, [])
+
   return (
     <View style={styles.container}>
-      <FinanceFilter currentSelected={select} setCurrentSelected={setSelect} filterData={PROFILE_MANAGEMENT_FILTER}/>
-      <AppText style={styles.text}>Có Tất Cả <AppText value={total} color={color.palette.blue} style={FONT_BOLD_12}/> Hồ Sơ</AppText>
-      <FlatList data={data} renderItem={renderItem} style={styles.flatList} onResponderEnd={loanStore.loadMoreRecords} onEndReachedThreshold={0.2}/>
+      <FinanceFilter currentSelected={select} setCurrentSelected={setSelect} filterData={PROFILE_MANAGEMENT_FILTER} />
+      <AppText style={styles.text}>Có Tất Cả <AppText value={total} color={color.palette.blue} style={FONT_BOLD_12} /> Hồ Sơ</AppText>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        style={styles.flatList}
+        onEndReached={() => {
+            loanStore.loadMoreRecords()
+            setLoadMore(false)
+        }}
+        onEndReachedThreshold={0.2}
+        onScrollBeginDrag={() => {
+          setLoadMore(false)
+        }}
+      />
+      {loadMore && <AppText style={styles.text} value={'Loanding...'}/>}
     </View>
   )
 });
@@ -34,10 +52,10 @@ const RecordsManagement = React.memo((props: Props) => {
 export default RecordsManagement;
 
 const styles = ScaledSheet.create({
-    container: {
-      backgroundColor: color.palette.white,
-      flex: 1,
-    },
+  container: {
+    backgroundColor: color.palette.white,
+    flex: 1,
+  },
   text: {
     fontSize: '12@ms',
     fontFamily: fontFamily.regular,
@@ -46,7 +64,7 @@ const styles = ScaledSheet.create({
     marginBottom: '5@s'
   },
   flatList: {
-      paddingHorizontal: '16@ms',
+    paddingHorizontal: '16@ms',
   },
 
 });
