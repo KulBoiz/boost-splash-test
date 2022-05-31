4e213a6563ee08c64d55a4f2e439d64917272fa5
Cao Thien <cvt3894@gmail.com>
2022-05-31 15:31:50 +0700

    fix: upload file

--- app/models/agent-store/agent-store.ts
+++ app/models/agent-store/agent-store.ts
@@ -1,7 +1,8 @@
 import { flow, Instance, SnapshotOut, types } from "mobx-state-tree"
 import { withEnvironment } from "../extensions/with-environment"
 import { UploadApi } from "../../services/api/upload-api"
-import mime from 'mime';
+import mime from "mime"
+import * as FileSystem from "expo-file-system"
 
 /**
  * Model description here for TypeScript hints.
@@ -10,56 +11,76 @@ export const AgentStoreModel = types
   .model("AgentStore")
   .extend(withEnvironment)
   .props({
-    fullName: types.optional(types.string, ''),
-    email: types.optional(types.string, ''),
-    phone: types.optional(types.string, ''),
-    bankNumber: types.optional(types.string, ''),
-    bankName: types.optional(types.string, ''),
-    bankBranch: types.optional(types.string, ''),
-    province: types.optional(types.string, ''),
-    district: types.optional(types.string, ''),
-    commune: types.optional(types.string, ''),
-    address: types.optional(types.string, ''),
-    sex: types.optional(types.string, ''),
-    citizenIdentification : types.optional(types.string, ''),
-    dateRange: types.optional(types.string, ''),
-    issuedBy: types.optional(types.string, ''),
+    fullName: types.optional(types.string, ""),
+    email: types.optional(types.string, ""),
+    phone: types.optional(types.string, ""),
+    bankNumber: types.optional(types.string, ""),
+    bankName: types.optional(types.string, ""),
+    bankBranch: types.optional(types.string, ""),
+    province: types.optional(types.string, ""),
+    district: types.optional(types.string, ""),
+    commune: types.optional(types.string, ""),
+    address: types.optional(types.string, ""),
+    sex: types.optional(types.string, ""),
+    citizenIdentification: types.optional(types.string, ""),
+    dateRange: types.optional(types.string, ""),
+    issuedBy: types.optional(types.string, ""),
   })
   .views((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
   .actions((self) => ({
-      userInfo: flow(function* userInfo(sex: string, email: string, phone: string, bankNumber: string,
-      bankName: string, bankBranch?: string, province: string, district: string, commune: string, address: string) {
-              self.sex = sex
-              self.email = email
-              self.phone = phone
-              self.bankNumber= bankNumber
-              self.bankName = bankName
-              self.bankBranch = bankBranch ?? ''
-              self.province = province
-              self.district = district
-              self.commune =  commune
-              self.address = address
-      }),
+    userInfo: flow(function* userInfo(
+      sex: string,
+      email: string,
+      phone: string,
+      bankNumber: string,
+      bankName: string,
+      bankBranch?: string,
+      province: string,
+      district: string,
+      commune: string,
+      address: string,
+    ) {
+      self.sex = sex
+      self.email = email
+      self.phone = phone
+      self.bankNumber = bankNumber
+      self.bankName = bankName
+      self.bankBranch = bankBranch ?? ""
+      self.province = province
+      self.district = district
+      self.commune = commune
+      self.address = address
+    }),
 
-      userId: flow(function* userId(fullName: string,citizenIdentification: string, dateRange: string, issuedBy: string) {
-              self.fullName = fullName
-              self.citizenIdentification = citizenIdentification
-              self.dateRange = dateRange
-              self.issuedBy = issuedBy
-      }),
+    userId: flow(function* userId(
+      fullName: string,
+      citizenIdentification: string,
+      dateRange: string,
+      issuedBy: string,
+    ) {
+      self.fullName = fullName
+      self.citizenIdentification = citizenIdentification
+      self.dateRange = dateRange
+      self.issuedBy = issuedBy
+    }),
 
     uploadFrontImage: flow(function* uploadFrontImage(path: string) {
       const uploadApi = new UploadApi(self.environment.api)
-      const formData = new FormData();
-        const file = {
-          uri: path.startsWith('ph://') ? `ph-upload${path.substring(2)}` : path,
-          name: path.substring(path.lastIndexOf('/') + 1, path.length),
-          filename: path.substring(path.lastIndexOf('/') + 1, path.length) + 'jpg',
-          type: mime.getType(path) ?? 'image/jpg' ,
-        };
-        formData.append('identification.frontPhoto', file);
+      const fileName = path.substring(path.lastIndexOf("/") + 1, path.length) + ".jpg"
+      const destPath = FileSystem.cacheDirectory + "/" + fileName
+      if (path.startsWith("assets") || path.startsWith("ph://")) {
+        yield FileSystem.copyAsync({ from: path, to: destPath })
+      }
+      const formData = new FormData()
+      const file = {
+        uri: destPath,
+        name: path.substring(path.lastIndexOf("/") + 1, path.length),
+        filename: fileName,
+        type: mime.getType(path) ?? "image/jpg",
+      }
+      formData.append("identification.frontPhoto", file)
 
-      console.log('formData', formData)
+      console.log("formData", file)
       const result = yield uploadApi.uploadFile(formData)
       if (result.kind !== "ok") {
         return result
@@ -72,14 +93,14 @@ export const AgentStoreModel = types
 
     uploadBackImage: flow(function* uploadBackImage(path: string) {
       const uploadApi = new UploadApi(self.environment.api)
-      const formData = new FormData();
+      const formData = new FormData()
       const file = {
-        uri: path.startsWith('ph://') ? `ph-upload${path.substring(2)}` : path,
-        name: path.substring(path.lastIndexOf('/') + 1, path.length),
-        filename: path.substring(path.lastIndexOf('/') + 1, path.length) + 'jpg',
+        uri: path.startsWith("ph://") ? `ph-upload${path.substring(2)}` : path,
+        name: path.substring(path.lastIndexOf("/") + 1, path.length),
+        filename: path.substring(path.lastIndexOf("/") + 1, path.length) + "jpg",
         type: mime.getType(path),
-      };
-      formData.append('identification.backSidePhoto', file);
+      }
+      formData.append("identification.backSidePhoto", file)
       const result = yield uploadApi.uploadFile(formData)
       if (result.kind !== "ok") {
         return result
@@ -89,7 +110,6 @@ export const AgentStoreModel = types
         data: result.data,
       }
     }),
-
   })) // eslint-disable-line @typescript-eslint/no-unused-vars
 
 type AgentStoreType = Instance<typeof AgentStoreModel>
--- app/screens/settting/setting-screen.tsx
+++ app/screens/settting/setting-screen.tsx
@@ -13,51 +13,57 @@ import VerifyUser from "./components/verify-user"
 import { AppText } from "../../components/app-text/AppText"
 import SettingAuthScreen from "../../components/app-niew-no-auth"
 
-interface Props{}
+interface Props {}
 
 const SettingScreen: FC<Props> = observer((props: Props) => {
   const navigation = useNavigation()
-  const {authStoreModel} = useStores()
-  const logout= async ()=> {
+  const { authStoreModel } = useStores()
+  const logout = async () => {
     await authStoreModel.logout()
     navigation.dispatch(StackActions.push(ScreenNames.AUTH))
   }
 
   const login = () => {
-    navigation.dispatch(StackActions.push(ScreenNames.AUTH))
+    navigation.dispatch(StackActions.push(ScreenNames.AGENT, { screen: ScreenNames.CAPTURE_ID }))
   }
 
   return (
     <View style={styles.container}>
-      <AppHeader headerTx={"header.personalSetting"}/>
-      {authStoreModel?.isLoggedIn ?
+      <AppHeader headerTx={"header.personalSetting"} />
+      {authStoreModel?.isLoggedIn ? (
         <ScrollView>
           <VerifyUser />
           {SETTING_LIST.map((value, index) => (
-            <SettingItem key={index.toString()} active={value.active} icon={value.icon} title={value.title} onPress={value.onPress} />
+            <SettingItem
+              key={index.toString()}
+              active={value.active}
+              icon={value.icon}
+              title={value.title}
+              onPress={value.onPress}
+            />
           ))}
-          <AppButton title={'Đăng xuất'} onPress={logout} />
+          <AppButton title={"Đăng xuất"} onPress={logout} />
 
           <View style={{ height: 100 }} />
         </ScrollView>
-        :
+      ) : (
         <SettingAuthScreen />
-        // <View style={styles.login}>
-        //   <AppText value={'Bạn cần phải đăng nhập để sử dụng tính năng này'}/>
-        //   <AppText value={'Đăng nhập'} underline onPress={login} color={color.palette.blue}/>
-        // </View>
-      }
+      )}
+      <View style={styles.login}>
+        <AppText value={"Bạn cần phải đăng nhập để sử dụng tính năng này"} />
+        <AppText value={"Đăng nhập"} underline onPress={login} color={color.palette.blue} />
+      </View>
     </View>
   )
-});
+})
 
-export default SettingScreen;
+export default SettingScreen
 
 const styles = StyleSheet.create({
-    container: {backgroundColor: color.palette.lightBlue, flex:1},
+  container: { backgroundColor: color.palette.lightBlue, flex: 1 },
   login: {
     alignItems: "center",
     flex: 1,
-    justifyContent: "center"
-  }
-});
+    justifyContent: "center",
+  },
+})
