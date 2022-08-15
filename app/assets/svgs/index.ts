6ad695b3edb08a5f0aaf8e9b8f5ccfe5a3612e73
Huy Trần <huytd@melisoft.vn>
2022-08-15 19:44:37 +0700

    empty list

--- app/assets/svgs/index.ts
+++ app/assets/svgs/index.ts
@@ -99,6 +99,7 @@ export { default as OrangePlusSvg } from "./orange_plus.svg"
 export { default as ListHospitalSvg } from "./list_hospital.svg"
 export { default as WarningSvg } from "./warning.svg"
 export { default as BlueTickSvg } from "./blue_tick.svg"
+export { default as NoDataSvg } from "./no-data.svg"
 
 // home
 export { default as AccountHomeActiveSvg } from "./account-home-active.svg"
