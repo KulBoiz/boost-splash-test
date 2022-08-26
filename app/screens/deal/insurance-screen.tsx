c2ffaf6b71b01ea11f3732fd0383b4004fdd2105
LuuVanTruong308 <48488891+LuuVanTruong308@users.noreply.github.com>
2022-08-26 22:42:58 +0700

    fix bug deal insurance

--- app/screens/deal/insurance-screen.tsx
+++ app/screens/deal/insurance-screen.tsx
@@ -1,11 +1,9 @@
-import { groupBy, map } from "lodash";
-import moment from "moment";
-import { Box, HStack, Spinner } from "native-base";
-import React, { useCallback, useMemo, useState } from "react";
-import { RefreshControl, SectionList } from 'react-native';
-import { s, ScaledSheet, vs } from "react-native-size-matters";
-import { Text } from "../../components";
+import { Box, Spinner } from "native-base";
+import React, { useCallback, useEffect, useState } from "react";
+import { FlatList, RefreshControl } from 'react-native';
+import { ScaledSheet } from "react-native-size-matters";
 import SettingAuthScreen from "../../components/app-view-no-auth";
+import EmptyList from "../../components/empty-list";
 import { useStores } from "../../models";
 import { navigate } from "../../navigators";
 import { ScreenNames } from "../../navigators/screen-names";
@@ -19,9 +17,24 @@ interface Props { }
 const InsuranceScreen = (props: Props) => {
   const { insuranceStore, authStoreModel } = useStores()
   const [tabSelect, setTabSelect] = useState("1")
+  const [loading, showLoading] = useState(false)
 
   const isListBuy = tabSelect === INSURANCE_TABS[0].key
   const isLoggedIn = authStoreModel.isLoggedIn
+
+  useEffect(() => {
+    fetchList()
+  }, [])
+
+  const fetchList = () => {
+    showLoading(true)
+    isListBuy ? insuranceStore.getListBuyInsurance({}, { page: 1, limit: 20 }).then(res => {
+      showLoading(false)
+    })
+      : insuranceStore.getListClaimInsurance({}, { page: 1, limit: 20 }).then(res => {
+        showLoading(false)
+      })
+  }
 
   const showDetail = useCallback((index) => {
     if (isListBuy) {
@@ -30,26 +43,15 @@ const InsuranceScreen = (props: Props) => {
     else navigate(ScreenNames.INSURANCE_CLAIM_DETAIL, { index })
   }, [tabSelect])
 
-  const data = useMemo(() => {
-    const dataGroup = groupBy(
-      map(isListBuy ? insuranceStore.listBuy : insuranceStore.listClaim, (item) => ({
-        ...item,
-        dateGroup: isListBuy ? (item?.transaction?.subType) : moment(item.createdAt).format("MM/YYYY"),
-      })),
-      "dateGroup",
-    )
-    const sections = Object.keys(dataGroup).map((key) => ({
-      data: dataGroup[key],
-      title: isListBuy ? `${key}` : `Tháng ${key}`,
-    }))
-
-    return sections
-  }, [insuranceStore.listBuy, insuranceStore.listClaim, tabSelect])
-
   const _onRefresh = useCallback(() => {
+    showLoading(true)
     isListBuy ?
-      insuranceStore.getListBuyInsurance({}, { page: 1, limit: 20 }, true)
-      : insuranceStore.getListClaimInsurance({}, { page: 1, limit: 20 }, true)
+      insuranceStore.getListBuyInsurance({}, { page: 1, limit: 20 }, true).then(res => {
+        showLoading(false)
+      })
+      : insuranceStore.getListClaimInsurance({}, { page: 1, limit: 20 }, true).then(res => {
+        showLoading(false)
+      })
   }, [isListBuy])
 
   const _onLoadMore = useCallback(() => {
@@ -61,7 +63,9 @@ const InsuranceScreen = (props: Props) => {
           {},
           { page: insuranceStore?.pagingListBuy?.page + 1, limit: 20 },
           false
-        )
+        ).then(res => {
+          showLoading(false)
+        })
       }
     }
     else {
@@ -72,39 +76,28 @@ const InsuranceScreen = (props: Props) => {
           {},
           { page: insuranceStore?.pagingListClaim?.page + 1, limit: 20 },
           false
-        )
+        ).then(res => {
+          showLoading(false)
+        })
       }
     }
   }, [insuranceStore.isLoadingMore, insuranceStore.listBuy, insuranceStore.listClaim])
 
   const onChangeTab = useCallback((key) => {
     setTabSelect(key)
-
-    isListBuy ?
-      insuranceStore.getListBuyInsurance({}, { page: 1, limit: 20 })
-      : insuranceStore.getListClaimInsurance({}, { page: 1, limit: 20 })
-
+    fetchList()
   }, [tabSelect])
-
-  const renderSectionHeader = useCallback(({ section: { title, data } }) => {
-    return (
-      <HStack alignItems="center" bg="white" px={s(16)} py={vs(6)}>
-        <Text fontWeight="600" fontSize="14" color="ebony" text={title} />
-        <Text fontWeight="500" fontSize="12" color="primary" text={` (${data?.length || 0})`} />
-      </HStack>
-    )
-  }, [])
 
   const renderItem = useCallback(({ item, index }) => {
     return <ManageInsuranceItem item={item} index={index} onPress={() => showDetail(index)} />
   }, [tabSelect])
 
   const ListFooterComponent = useCallback(() => {
-    if (insuranceStore.isLoadingMore) {
+    if (loading) {
       return <Spinner color="primary" m="4" />
     }
     return <Box m="4" />
-  }, [insuranceStore.isLoadingMore])
+  }, [loading])
 
   return (
     <>
@@ -114,12 +107,10 @@ const InsuranceScreen = (props: Props) => {
           <ManageInsuranceTab onChangeTab={onChangeTab} tabSelect={tabSelect} />
           <Box flex={1} bg="white" pt='1'>
 
-            <SectionList
-              sections={data}
+            <FlatList
+              data={isListBuy ? insuranceStore.listBuy : insuranceStore.listClaim}
               keyExtractor={(_, index) => index.toString()}
               renderItem={renderItem}
-              renderSectionHeader={renderSectionHeader}
-              stickySectionHeadersEnabled
               refreshControl={
                 <RefreshControl
                   refreshing={insuranceStore.isRefreshing}
@@ -131,6 +122,8 @@ const InsuranceScreen = (props: Props) => {
               onEndReachedThreshold={0.2}
               onEndReached={_onLoadMore}
               ListFooterComponent={ListFooterComponent}
+              contentContainerStyle={styles.list}
+              ListEmptyComponent={!loading ? EmptyList : <></>}
             />
           </Box>
         </Box> :
@@ -145,4 +138,7 @@ export default InsuranceScreen;
 const styles = ScaledSheet.create({
   container: { backgroundColor: color.palette.white, flex: 1 },
   header: { backgroundColor: "white", borderBottomWidth: 0 },
+  list: {
+    paddingVertical: '16@s'
+  }
 });
